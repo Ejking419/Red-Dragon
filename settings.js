3245f8fd67e39e6605c396e929aa42a5e029f839
12cfgh <112168182+12cfgh@users.noreply.github.com>
2022-09-08 09:52:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,17 +29,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
+global.vcardowner = ['+94 76 700 1095] // owner number
 global.ownername = "🍪🍬  ϻｉηỮℝᗩ  💙♙" // owner name
 global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
+global.socialm = minuragit" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
 global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
-global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
+global.callblock = "false" // call block "flase/ "false" ("true"  to block callers)
 global.antibad = "false" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
 global.antilink = "false" // To remove group link sharing peoples
